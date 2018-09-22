bca124f942c63e2324703c93bdef0da6d7f1d75a
Martinkevich Yana <Yana22685@yandex.ru>
2018-09-23 00:38:36 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+
+   var t = Number(temperature);
+  if(t != 0) {
+  	return (t*9)/5 + 32;
+  }
+  else {
+  	return 32;
+  }
 };
